5c407ffdac71c0bcba7fc1f064498a6cc4a62af0
jayanth <koppakajayanthkumar2000@gmail.com>
2024-07-04 19:01:26 +0530

    Added EmployeeOperations.java

---
+++ EmployeeOperations.java
@@ -0,0 +1,7 @@
+public abstract class  EmployeeOperations {
+   abstract void addemployee(EmployeeManagement employeeManagement);
+    abstract void removeemployeemanagement(int employeeID);
+    abstract void updateEmployeemangement(EmployeeManagement employeeManagement );
+    abstract void displayemployeemanagement();
+
+}
